e22a129a908ec27ee688731015eddc544ccfed5d
Franco Aguzzi <franco@MacBook-Pro-de-Franco.local>
2022-07-31 13:05:33 -0300

    finished 'in construction' homepage styling

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -4,16 +4,16 @@
 
 <script>
 
-import { defineComponent } from 'vue'
+import { defineComponent, mounted } from 'vue'
 
 export default defineComponent({
     name: 'Image',
     props: {
         src: { type: String, default: '' },
         alt: { type: String, default: '' },
-        width: { type: String, default: '0' },
+        width: { type: String, default: '0px' },
         height: { type: String, default: 'auto' },
-    }
+    },
 })
 
 </script>
